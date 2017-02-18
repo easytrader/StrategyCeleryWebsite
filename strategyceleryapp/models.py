76341421dcbbc6d4e899844bb25a60ec2e15318d
leo <leoyenschen@gmail.com>
2017-02-18 15:24:06 +0800

    add daily_pric table

--- strategyceleryapp/models.py
+++ strategyceleryapp/models.py
@@ -17,3 +17,22 @@ class Strategy(models.Model):
     strategy_name = models.CharField(max_length=20)
     strategy = models.TextField()
     position = models.TextField()
+
+
+class symbol(models.Model):
+    ticker = models.CharField(max_length=40)
+    name = models.CharField(max_length=200)
+    created_date = models.DateTimeField(auto_now=True)
+    last_updated_date = models.DateTimeField(auto_now=True)
+
+class daily_price(models.Model):
+    symbol_id = models.IntegerField()
+    price_date = models.DateTimeField()
+    created_date = models.DateTimeField()
+    last_updated_date = models.DateTimeField()
+    open_price = models.DecimalField(max_digits=19, decimal_places=10)
+    high_price = models.DecimalField(max_digits=19, decimal_places=10)
+    low_price = models.DecimalField(max_digits=19, decimal_places=10)
+    close_price = models.DecimalField(max_digits=19, decimal_places=10)
+    adj_close_price = models.DecimalField(max_digits=19, decimal_places=10)
+    volume = models.BigIntegerField()
