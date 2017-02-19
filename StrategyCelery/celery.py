55b28371c965508e55bdcc80c22f20056f2c41a7
leo <leoyenschen@gmail.com>
2017-02-19 11:38:14 +0800

    add author

--- StrategyCelery/celery.py
+++ StrategyCelery/celery.py
@@ -1,3 +1,9 @@
+# @Author: Chen yunsheng(Leo YS CHen)
+# @Location: Taiwan
+# @E-mail:leoyenschen@gmail.com
+# @Date:   2017-02-14 00:11:27
+# @Last Modified by:   Chen yunsheng
+
 from __future__ import absolute_import
 
 import os
--- strategyceleryapp/tasks.py
+++ strategyceleryapp/tasks.py
@@ -1,3 +1,9 @@
+# @Author: Chen yunsheng(Leo YS CHen)
+# @Location: Taiwan
+# @E-mail:leoyenschen@gmail.com
+# @Date:   2017-02-14 00:11:27
+# @Last Modified by:   Chen yunsheng
+
 from StrategyCelery.celery import app
 from .models import daily_price
 
